bd0551ae017db57ea44cd54d10ab41b6b7d59dbf
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-01 22:36:56 +0530

    changes

--- roboshop1.sh
+++ roboshop1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 SG_ID="sg-0b4c1bffcd0783883"
 AMI_ID="ami-0220d79f3f480ecf5"
-for INSTANCE in $@
+for instance in $@
 do
     instance_id=$(
          aws ec2 run-instances \
-    --image-id ami-0220d79f3f480ecf5 \
+    --image-id $AMI_ID \
     --instance-type t3.micro \
-    --security-group-ids sg-0b4c1bffcd0783883 \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]' \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text)
 
