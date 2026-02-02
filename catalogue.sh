bcbba5aeb69ab08d456567cddc147b656a6ccb20
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-02 19:32:05 +0530

    changes

--- catalogue.sh
+++ catalogue.sh
@@ -78,7 +78,6 @@ INDEX=$(mongosh --host $mongodb_host --quiet  --eval 'db.getMongo().getDBNames()
 if [ $INDEX -le 0 ]; then
 
     mongosh --host $MONGODB_HOST </app/db/master-data.js
-
     VALIDATE $? "Loading products"
 
 else
