3ddfae3e7d06805fa9f391f0e86a02cfb2484c05
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-02 16:02:44 +0530

    changes

--- roboshop1.sh
+++ roboshop1.sh
@@ -5,59 +5,59 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z067791029EEJ0FAK30QG"
 DOMAIN_NAME="devopswithsai.online"
 
-for instance in "$@"
+for instance in $@
 do
-  instance_id=$(
-    aws ec2 run-instances \
-      --image-id "$AMI_ID" \
-      --instance-type t3.micro \
-      --security-group-ids "$SG_ID" \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-      --query 'Instances[0].InstanceId' \
-      --output text
-  )
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
 
-  echo "Launched instance: $instance_id"
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+    fi
 
-  sleep 10
+    echo "IP Address: $IP"
 
-  if [ "$instance" = "robo" ]; then
-    IP=$(aws ec2 describe-instances \
-      --instance-ids "$instance_id" \
-      --query 'Reservations[].Instances[].PublicIpAddress' \
-      --output text)
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
 
-    RECORD_NAME="$DOMAIN_NAME"
-  else
-    IP=$(aws ec2 describe-instances \
-      --instance-ids "$instance_id" \
-      --query 'Reservations[].Instances[].PrivateIpAddress' \
-      --output text)
+    echo "record updated for $instance"
 
-    RECORD_NAME="$instance.$DOMAIN_NAME"
-  fi
-
-  echo "IP address: $IP"
-  echo "Creating record: $RECORD_NAME"
-
-  aws route53 change-resource-record-sets \
-    --hosted-zone-id "$ZONE_ID" \
-    --change-batch "{
-      \"Comment\": \"Update A record\",
-      \"Changes\": [
-        {
-          \"Action\": \"UPSERT\",
-          \"ResourceRecordSet\": {
-            \"Name\": \"$RECORD_NAME\",
-            \"Type\": \"A\",
-            \"TTL\": 1,
-            \"ResourceRecords\": [
-              { \"Value\": \"$IP\" }
-            ]
-          }
-        }
-      ]
-    }"
-
-  echo "Record updated for $instance"
 done
