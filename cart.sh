cd63151425d88a72940c307bfb48609b835d6ff0
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-03 17:12:09 +0530

    changes

--- cart.sh
+++ cart.sh
@@ -1,67 +1,81 @@
 #!/bin/bash
 user_id=$(id -u)
-log_folder="/var/log/shell-script/"
+log_folder="/var/log/shell_folder/"
 log_file="$log_folder/$0.log"
 R="\e[31m"
-G="\e[32m"
+G="\e[32m" 
 Y="\e[33m"
-B="\e[34m"
+B="\e[34m" 
 N="\e[0m"
 SCRIPT_DIR=$PWD
-redis_host_name="redis.devopswithsai.online"
+mongodb_host_id="mongodb.devopswithsai.online"
+redis_host_id="redis.devopswithsai.online"
 
 if [ $user_id -ne 0 ];then
-echo -e "$R please enter with root access $N" &>>$log_file
+echo -e "$R Please enter with root access $N"
 exit 1
+fi
 
 mkdir -p $log_folder
- validate(){
+validate(){
     if [ $1 -ne 0 ];then
-    echo -e "$2 $R failed $N"
+    echo -e "$2 is $R failed $N"
     else
-    echo -e "$2 $R success $N"
-    fi 
- }
+    echo -e "$2 is $G success $N"
+    fi
+}
 
-dnf module disable nodejs -y &>>$log_file
+dnf module disable nodejs -y  &>>$log_file
 dnf module enable nodejs:20 -y &>>$log_file
-validate $? "disabled and enabled"
+validate $? "disabling and enabling nodejs"
 
 dnf install nodejs -y &>>$log_file
-validate $? "installed nodejs"
+validate $? "installing nodejs"
+
 
 id roboshop
 if [ $? -ne 0 ];then
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$log_file
-validate $? "user added"
+cartadd --system --home /app --shell /sbin/nologin --comment "roboshop system cart" roboshop &>>$log_file
+validate $? "cart added"
 else
-echo "user already exist"
+echo "cart already exist"
 fi
 
-mkdir -p /app &>>$log_file
-validate $? "creating app directory"
+mkdir /app &>>$log_file
+validate $? "app directory created"
 
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$log_file
 validate $? "downloaded the cart content"
 
+
 cd /app &>>$log_file
+validate $? "Go to app directory"
 
 rm -rf */app/ &>>$log_file
-validate $? "removed the old content"
+validate $? "removed existing data"
 
 unzip /tmp/cart.zip &>>$log_file
-validate $? "unzipped the cart content"
+validate $? "unzipped the content to access"
 
 cd /app &>>$log_file
 npm install &>>$log_file
-validate $? "unzipped the cart content"
+validate $? "npm dependencies installed"
 
 cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service &>>$log_file
-validate $? "copied the cart service"
+validate $? "copied cart service"
 
 systemctl daemon-reload &>>$log_file
-validate $? "deamon-reloaded"
+validate $? "daemon-reloaded"
 
 systemctl enable cart &>>$log_file
 systemctl start cart &>>$log_file
-validate $? "enable and disable"
+validate $? "ebable and disable"
+
+
+
+
+
+
+
+
+
--- roboshop.sh
+++ roboshop.sh
@@ -384,4 +384,158 @@ systemctl enable redis &>>$LOGS_FILE
 systemctl start redis 
 VALIDATE $? "Enabled and started Redis"
 
+____________________
+#!/bin/bash
+user_id=$(id -u)
+log_folder="/var/log/shell_folder/"
+log_file="$log_folder/$0.log"
+R="\e[31m"
+G="\e[32m" 
+Y="\e[33m"
+B="\e[34m" 
+N="\e[0m"
+SCRIPT_DIR=$PWD
+mongodb_host_id="mongodb.devopswithsai.online"
+redis_host_id="redis.devopswithsai.online"
+
+if [ $user_id -ne 0 ];then
+echo -e "$R Please enter with root access $N"
+exit 1
+fi
+
+mkdir -p $log_folder
+validate(){
+    if [ $1 -ne 0 ];then
+    echo -e "$2 is $R failed $N"
+    else
+    echo -e "$2 is $G success $N"
+    fi
+}
+
+dnf module disable nodejs -y  &>>$log_file
+dnf module enable nodejs:20 -y &>>$log_file
+validate $? "disabling and enabling nodejs"
+
+dnf install nodejs -y &>>$log_file
+validate $? "installing nodejs"
+
+
+id roboshop
+if [ $? -ne 0 ];then
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$log_file
+validate $? "user added"
+else
+echo "user already exist"
+fi
+
+mkdir /app &>>$log_file
+validate $? "app directory created"
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$log_file
+validate $? "downloaded the user content"
+
+
+cd /app &>>$log_file
+validate $? "Go to app directory"
+
+rm -rf */app/ &>>$log_file
+validate $? "removed existing data"
+
+unzip /tmp/user.zip &>>$log_file
+validate $? "unzipped the content to access"
+
+cd /app &>>$log_file
+npm install &>>$log_file
+validate $? "npm dependencies installed"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service &>>$log_file
+validate $? "copied user service"
+
+systemctl daemon-reload &>>$log_file
+validate $? "daemon-reloaded"
+
+systemctl enable user &>>$log_file
+systemctl start user &>>$log_file
+validate $? "ebable and disable"
+
+
+____________________
+#!/bin/bash
+user_id=$(id -u)
+log_folder="/var/log/shell-script/"
+log_file="$log_folder/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+redis_host_name="redis.devopswithsai.online"
+
+if [ $user_id -ne 0 ];then
+echo -e "$R please enter with root access $N" &>>$log_file
+exit 1
+
+mkdir -p $log_folder
+ validate(){
+    if [ $1 -ne 0 ];then
+    echo -e "$2 $R failed $N"
+    else
+    echo -e "$2 $R success $N"
+    fi 
+ }
+
+dnf module disable nodejs -y &>>$log_file
+dnf module enable nodejs:20 -y &>>$log_file
+validate $? "disabled and enabled"
+
+dnf install nodejs -y &>>$log_file
+validate $? "installed nodejs"
+
+id roboshop
+if [ $? -ne 0 ];then
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$log_file
+validate $? "user added"
+else
+echo "user already exist"
+fi
+
+mkdir -p /app &>>$log_file
+validate $? "creating app directory"
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$log_file
+validate $? "downloaded the cart content"
+
+cd /app &>>$log_file
+
+rm -rf */app/ &>>$log_file
+validate $? "removed the old content"
+
+unzip /tmp/cart.zip &>>$log_file
+validate $? "unzipped the cart content"
+
+cd /app &>>$log_file
+npm install &>>$log_file
+validate $? "unzipped the cart content"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service &>>$log_file
+validate $? "copied the cart service"
+
+systemctl daemon-reload &>>$log_file
+validate $? "deamon-reloaded"
+
+systemctl enable cart &>>$log_file
+systemctl start cart &>>$log_file
+validate $? "enable and disable"
+
+
+
+
+
+
+
+
+
+
+
     
